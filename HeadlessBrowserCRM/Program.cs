77032e50236d24e938403e9584e25f7bb56af626
Kyle Reinertson <kyle.reinertson@mural365.com>
2018-11-12 09:56:03 -0700

    Final Commit

--- HeadlessBrowserCRM/Program.cs
+++ HeadlessBrowserCRM/Program.cs
@@ -4,17 +4,8 @@ using Microsoft.Xrm.Sdk.Client;
 using Microsoft.Crm.Sdk.Messages;
 using System.Net;
 using System.ServiceModel.Description;
-using System.IO;
-using System.Text;
-using Microsoft.Xrm.Sdk.Query;
-using System.Linq;
-using System.Net.Mail;
-using System.Collections.Generic;
-using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Firefox;
-using System.Threading;
 using OpenQA.Selenium.Support.UI;
 
 
@@ -24,23 +15,23 @@ namespace HeadlessBrowserCRM
     {
 
         private static Guid moduserGuid = new Guid("089824D8-0059-47D3-B6DD-D7B7DA317A26");
-        static string FROM = "admin@CRM108762.onmicrosoft.com";
-        static string FROMNAME = "CRM Online User";
-        static string TO = "kyle.reinertson@mural365.com";
-        static string SMTP_USERNAME = "admin@CRM108762.onmicrosoft.com";
-        static string SMTP_PASSWORD = "kyle.reiner@2364";
-        static string CONFIGSET = "ConfigSet";
-        static string HOST = "smtp.office365.com";
-        static int PORT = 587;
-        static string username = "admin@CRM108762.onmicrosoft.com";
-        static string password = "kyle.reiner@2364";
-        static string url = "https://crm108762.api.crm.dynamics.com/XRMServices/2011/Organization.svc";
-        static string path = @"C:\Users\kyle.reinertson\source\repos\TryThis.csv";
-        static string html = "C:\\Users\\kyle.reinertson\\source\\repos\\CRMConsoleActivities\\CRMConsoleActivities\\HTMLPage1.html";
-        static string muralPath = "https://crm108762.crm.dynamics.com/main.aspx?forceClassic=1";
-        static string O365loginButtonId = "idSIButton9";
-        static string O365usernameId = "i0116";
-        static string O365passwordId = "i0118";
+        private static string FROM = "admin@CRM108762.onmicrosoft.com";
+        private static string FROMNAME = "CRM Online User";
+        private static string TO = "kyle.reinertson@mural365.com";
+        private static string SMTP_USERNAME = "admin@CRM108762.onmicrosoft.com";
+        private static string SMTP_PASSWORD = "kyle.reiner@2364";
+        private static string CONFIGSET = "ConfigSet";
+        private static string HOST = "smtp.office365.com";
+        private static int PORT = 587;
+        private static string username = "admin@CRM108762.onmicrosoft.com";
+        private static string password = "kyle.reiner@2364";
+        private static string url = "https://crm108762.api.crm.dynamics.com/XRMServices/2011/Organization.svc";
+        private static string path = @"C:\Users\kyle.reinertson\source\repos\TryThis.csv";
+        private static string html = "C:\\Users\\kyle.reinertson\\source\\repos\\CRMConsoleActivities\\CRMConsoleActivities\\HTMLPage1.html";
+        private static string muralPath = "https://crm108762.crm.dynamics.com/main.aspx?forceClassic=1";
+        private static string O365loginButtonId = "idSIButton9";
+        private static string O365usernameId = "i0116";
+        private static string O365passwordId = "i0118";
 
 
 
@@ -107,27 +98,38 @@ namespace HeadlessBrowserCRM
             IWebElement query2 = driver.FindElement(By.Id(O365passwordId));
             query2.SendKeys(password);
             query2 = driver.FindElement(By.Id(O365loginButtonId));
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
             By locator = By.Id(O365loginButtonId);
             wait.Until(drv => By.Id(O365loginButtonId));
             query2.Click();
 
             IWebElement query3 = driver.FindElement(By.Id(O365loginButtonId));
             query3 = driver.FindElement(By.Id(O365loginButtonId));
+            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            By locator2 = By.Id(O365loginButtonId);
+            wait.Until(drv => By.Id(O365loginButtonId));
             query3.Click();
 
             IWebElement query4 = driver.FindElement(By.Id("TabSFA"));
             query4 = driver.FindElement(By.Id("TabSFA"));
+            WebDriverWait wait3 = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            By locator3 = By.Id("TabSFA");
+            wait.Until(drv => By.Id("TabSFA"));
             query4.Click();
 
             IWebElement query5 = driver.FindElement(By.Id("Settings"));
             query5 = driver.FindElement(By.Id("Settings"));
+            WebDriverWait wait4 = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            By locator4 = By.Id("Settings");
+            wait.Until(drv => By.Id("Settings"));
             query5.Click();
 
             IWebElement query6 = driver.FindElement(By.Id("nav_security"));
             query6 = driver.FindElement(By.Id("nav_security"));
+            WebDriverWait wait5 = new WebDriverWait(driver, TimeSpan.FromSeconds(120));
+            By locator5 = By.Id("nav_security");
+            wait.Until(drv => By.Id("nav_security"));
             query6.Click();
-
 
         }
     }
